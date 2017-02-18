a7f3e674159cea37de44674959556c67bee3f3a9
Michael Smith <mikejsmitty@gmail.com>
2017-02-17 22:46:16 -0600

    Refactored config code and added handlers

--- jinx/main.go
+++ jinx/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -16,39 +16,35 @@ import (
 )
 
 type config struct {
+	userIP string
+
 	BastionIP string
 	CertFile  string
+	Insecure  bool
 	PubKey    string
 	SSHUser   string
 	Timeout   int
 	URL       string
-	UserIP    string
 }
 
 func main() {
-	// Read config into a struct
-	var conf config
-	err := viper.Unmarshal(&conf)
+	// Process/load our config options
+	conf, err := getConf()
 	if err != nil {
-		log.Fatalf("Unable to read config into struct: %v", err)
-	}
-	// Verify config options
-	// FIXME Need to revisit this
-	if conf.BastionIP == "" {
-		log.Fatalf("bastionip is a required configuration field")
-	}
-	if conf.PubKey == "" {
-		log.Fatalf("pubkey is a required configuration field")
-	}
-	// FIXME Need to revisit this
-	if conf.UserIP == "" {
-		log.Fatalf("userip is a required configuration field")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
 	// Read in our pubkey file
 	pubKey, err := ioutil.ReadFile(conf.PubKey)
 	if err != nil {
-		log.Fatalf("Failed to read PubKey file: %v", err)
+		fmt.Fprintln(os.Stderr, "Failed to read PubKey file: %v", err)
+		os.Exit(1)
+	}
+
+	// Nag-mode for inadvertent/malicious insecure setting
+	if conf.Insecure {
+		fmt.Fprintln(os.Stderr, "Warning, your password is about to be sent insecurely. ctrl+c to quit")
 	}
 
 	// Read in our username and password
@@ -56,21 +52,27 @@ func main() {
 	fmt.Printf("Username: ")
 	user, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatalf("Input error: %v", err)
+		fmt.Fprintln(os.Stderr, "Input error: %v", err)
+		os.Exit(1)
 	}
 	user = strings.TrimSpace(user)
 
 	pass, err := speakeasy.Ask("Password: ")
 	if err != nil {
-		log.Fatalf("Shell error: %v", err)
+		fmt.Fprintln(os.Stderr, "Shell error: %v", err)
+		os.Exit(1)
 	}
 
 	/* Using basic auth for the initial prototype, since presumably this SSL certificate will be valid and
 	relatively insusceptible to MITM. Also, the digest auth client libraries I've seen are kinda bad.
 	I plan to come back and try writing a digest library once I get the prototype functional (and not in
 	a time crunch to make a demo). */
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: conf.Insecure},
+	}
 	client := &http.Client{
-		Timeout: time.Duration(conf.Timeout) * time.Second,
+		Transport: tr,
+		Timeout:   time.Duration(conf.Timeout) * time.Second,
 	}
 
 	// Assemble our POST form values
@@ -78,7 +80,7 @@ func main() {
 	form.Add("bastionIP", conf.BastionIP)
 	form.Add("key", string(pubKey))
 	form.Add("remoteUser", conf.SSHUser)
-	form.Add("userIP", conf.UserIP)
+	form.Add("userIP", conf.userIP)
 
 	req, err := http.NewRequest("POST", conf.URL, strings.NewReader(form.Encode()))
 	req.SetBasicAuth(user, pass)
@@ -86,39 +88,88 @@ func main() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Connection failed: %v", err)
+		fmt.Fprintln(os.Stderr, "Connection failed: %v", err)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Failed to process response: %v", err)
+		fmt.Fprintln(os.Stderr, "Failed to process response: %v", err)
+		os.Exit(1)
 	}
 
-	//fmt.Println(string(respBody))
-
-	err = ioutil.WriteFile(conf.CertFile, respBody, 0644)
-	if err != nil {
-		log.Fatal("Failed to write cert file: %v", err)
+	if resp.StatusCode == 200 {
+		err = ioutil.WriteFile(conf.CertFile, respBody, 0600)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to write cert file: %v", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, string(respBody))
+		os.Exit(resp.StatusCode)
 	}
 }
 
 func init() {
-	viper.SetConfigName(".jinx") // name of config file (without extension)
-	viper.AddConfigPath("/etc/jinx")
+	viper.SetConfigName(".jinx")     // name of config file (without extension)
+	viper.AddConfigPath("/etc/jinx") // /etc/jinx/.jinx.{yaml,toml,json} makes little sense. FIXME
 	viper.AddConfigPath("$HOME")
 	viper.ReadInConfig()
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		log.Printf("Using config file: %s", viper.ConfigFileUsed())
+		//log.Printf("Using config file: %s", viper.ConfigFileUsed())
 	}
 
-	viper.SetDefault("bastionip", "")   // FIXME Need to revisit this
-	viper.SetDefault("certfile", "")    // FIXME Need to revisit this
+	viper.SetDefault("bastionip", "")
+	viper.SetDefault("certfile", "") // FIXME Need to revisit this
+	viper.SetDefault("insecure", false)
 	viper.SetDefault("pubkey", "")      // FIXME Need to revisit this
-	viper.SetDefault("sshuser", "root") // FIXME Need to revisit this
+	viper.SetDefault("sshuser", "root") // FIXME Need to revisit this?
 	viper.SetDefault("timeout", 30)
-	viper.SetDefault("url", "https://127.0.0.1/")
-	viper.SetDefault("userip", "1.1.1.1") // FIXME Need to revisit this
+	viper.SetDefault("url", "https://localhost/")
+}
+
+func getConf() (*config, error) {
+	// Read config into a struct
+	var conf config
+	err := viper.Unmarshal(&conf)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to process config: %v", err)
+	}
+
+	// Verify config options
+	if conf.BastionIP == "" {
+		conf.BastionIP, _ = getBastionIP()
+		if conf.BastionIP == "" {
+			return nil, fmt.Errorf("could not find server's public IP. bastionip field required")
+		}
+	}
+	if conf.PubKey == "" {
+		return nil, fmt.Errorf("pubkey is a required configuration field")
+	}
+
+	// Check for non-SSL URL configuration (for warning)
+	if strings.HasPrefix(conf.URL, "http://") {
+		conf.Insecure = true
+	}
+
+	// Try to get the user's local IP from env variables
+	sc := os.Getenv("SSH_CLIENT")
+	scs := strings.Split(sc, " ")
+	if len(scs) > 0 {
+		conf.userIP = scs[0]
+	}
+	sc = os.Getenv("SSH_CONNECTION")
+	scs = strings.Split(sc, " ")
+	if conf.userIP == "" && len(scs) != 0 {
+		fmt.Printf("%s\n", scs[0])
+		conf.userIP = scs[0]
+	}
+	if conf.userIP == "" {
+		conf.userIP = "IP missing"
+	}
+
+	return &conf, nil
 }
